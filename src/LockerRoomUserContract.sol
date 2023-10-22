849c45da3d7b35c2f77106554549cce1cc3b8b5a
guzus <storminggalaxys3@gmail.com>
2023-10-22 15:30:08 +0900

    fix bug

--- src/LockerRoomUserContract.sol
+++ src/LockerRoomUserContract.sol
@@ -1,36 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.0;
 
 import "solmate/auth/Owned.sol";
+import "./LockerRoom.sol";
 
 contract LockerRoomUserContract is Owned {
-    address public lockerRoom;
+    LockerRoom public lockerRoom;
 
-    constructor(address _lockerRoom) {
-        lockerRoom = _lockerRoom;
+    constructor(address _lockerRoom, address _owner) Owned(_owner) {
+        lockerRoom = LockerRoom(_lockerRoom);
     }
 
-    function callByCode(uint8 code) {
+    function callByCode(uint8 code) external {
         bytes memory data = LockerRoom(lockerRoom).getCalldata(code);
         address(this).delegatecall(data);
     }
 
-    function callByCodeAndSuffix(uint8 code, bytes suffix) {
+    function callByCodeAndSuffix(uint8 code, bytes calldata suffix) external {
         bytes memory data = LockerRoom(lockerRoom).getCalldata(code);
         address(this).delegatecall(abi.encodePacked(data, suffix));
     }
 
-    function someFunction(uint256 a, uint256 b, address c, bytes calldata d) {
+    function someFunction(uint256 a, uint256 b, address c, bytes calldata d) public {
         // do something
     }
 
     // only accessible by admin is heavily recommended
 
-    function updateLockerRoom(address newLockerRoom) onlyOwner {
-        lockerRoom = newLockerRoom;
+    function updateLockerRoom(address newLockerRoom) external onlyOwner {
+        lockerRoom = LockerRoom(newLockerRoom);
     }
 
-    function addCode(uint8 code, bytes calldata data) onlyOwner {
+    function addCode(uint8 code, bytes calldata data) external onlyOwner {
         LockerRoom(lockerRoom).registerCalldata(code, data);
     }
 }
--- test/LockerRoom.t.sol
+++ test/LockerRoom.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
 import "../src/LockerRoomUserContract.sol";
@@ -11,16 +11,18 @@ contract LockerRoomTest is Test {
 
     function setUp() public {
         lockerRoom = new LockerRoom();
-        lockerRoomUserContract = new LockerRoomUserContract(lockerRoom);
+        lockerRoomUserContract = new LockerRoomUserContract(address(lockerRoom), address(this));
+        bytes memory data = abi.encodeWithSignature("someFunction(uint256,uint256,address,bytes)", 1, 2, address(this), bytes("gm"));
+        lockerRoom.registerCalldata(1, data);
     }
 
     function testCallByRawCalldata() public {
-        lockerRoom.someFunction(1, 2, address(this), 0x12345678);
+        // TODO: call with regular user
+        lockerRoomUserContract.someFunction(1, 2, address(this), bytes("gm"));
     }
 
     function testCallByCode() public {
-        bytes memory data = abi.encodeWithSignature("someFunction(uint256,uint256,address,bytes)", 1, 2, address(this), 0x12345678);
-        lockerRoom.registerCalldata(1, data);
+        // TODO: call with regular user
         lockerRoomUserContract.callByCode(1);
     }
 }
